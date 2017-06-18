173fe2cfc90623fcb093e1fd0225626ec486b655
Mustafa Saeed <mustafasaeed202019@gmail.com>
2017-06-18 17:34:43 +0200

    FInal Session

---
+++ Session Eight/LastDemo/LastDemo/Controllers/PostsController.cs
@@ -0,0 +1,65 @@
+﻿using LastDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LastDemo.Controllers
+{
+    public class PostsController : Controller
+    {
+
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        public ActionResult AllPosts()
+        {
+            var posts = _db.DbPosts.ToList();
+            return View(posts);
+        }
+
+        public ActionResult Post(int id)
+        {
+            return View(_db.DbPosts.Find(id));
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.DbPosts.Add(post);
+                _db.SaveChanges();
+                return RedirectToAction("AllPosts"); 
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var post = _db.DbPosts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeletePost(int id)
+        {
+            var post = _db.DbPosts.Find(id);
+            _db.DbPosts.Remove(post);
+            _db.SaveChanges();
+            return RedirectToAction("AllPosts");
+        }
+    }
+}
---
+++ Session Eight/LastDemo/LastDemo/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LastDemo.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Body { get; set; }
+        public bool Pubished { get; set; }
+        public DateTime PostedOn { get; set; }
+    }
+}
